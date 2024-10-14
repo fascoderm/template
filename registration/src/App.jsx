ff358f013dc168db5a72451a6254938ae469e8d5
Faiz <bakrefaiz@gmail.com>
2024-10-14 17:08:05 +0100

    version 1.0 of the template created

---
+++ registration/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl bg-black text-white'>
+        You are welcome to the project template, you can now begin to modify...
+      </h1>
+    </>
+  )
+}
+
+export default App
